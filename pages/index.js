4659e9b147862998b966c1e343d57a16ea1cd588
Pavel Khokhlov <ya.pavel-kh@yandex.ru>
2021-01-02 21:52:41 +0300

    fix: block scroll if open popup

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,16 @@
 const btnMenu = document.querySelectorAll(".button-menu");
 const popupMenu = document.querySelector(".popup-menu");
+const popupSale = document.querySelector(".popup-sale");
 const btnClosePopup = document.querySelectorAll(".popup-close");
 
 const handleMenuClick = () => {
   popupMenu.classList.add("popup_opened");
+  document.body.style.overflow = "hidden"; // OFF SCROLL
 };
 
 const handleCloseClick = (e) => {
   e.target.closest(".popup").classList.remove("popup_opened");
+  document.body.style.overflow = "visible"; // ON SCROLL
 };
 
 btnMenu.forEach((btn) => {
@@ -19,3 +22,10 @@ btnClosePopup.forEach((btn) => {
     handleCloseClick(e);
   });
 });
+
+const openPopupSale = () => {
+  popupSale.classList.add("popup_opened");
+  document.body.style.overflow = "hidden"; // OFF SCROLL
+};
+
+openPopupSale();
